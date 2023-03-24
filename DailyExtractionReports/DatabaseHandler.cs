16b66f29250f5b6d980200c803200cb7ba7a8209
M246830 <mills.paul@mayo.edu>
2023-03-24 09:14:41 -0400

    removed directive

--- DailyExtractionReports/DatabaseHandler.cs
+++ DailyExtractionReports/DatabaseHandler.cs
@@ -1,6 +1,5 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
-using System.Diagnostics.CodeAnalysis;
 using DailyExtractionReports.Models;
 using Dapper;
 using Microsoft.Extensions.Configuration;
